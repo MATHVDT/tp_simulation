059dd2d611d8464a78472db00ddde9ca30fbae6b
mavilledie4 <mathieu.villedieu_de_torcy@etu.uca.fr>
2021-10-25 13:43:59 +0200

    ajout tp4.c_h base sur tp prec

--- tp4/tp4.c
+++ tp4/tp4.c
@@ -1,4 +1,11 @@
 /* 
+TP Simulation
+Lab # 3Monte Carlo Simulation & Confidence Intervals
+
+VILLEDIEU DE TORCY Mathieu
+ZZ2 F 2 G22
+*/
+/* 
    A C-program for MT19937, with initialization improved 2002/1/26.
    Coded by Takuji Nishimura and Makoto Matsumoto.
 
@@ -41,9 +48,7 @@
    email: m-mat @ math.sci.hiroshima-u.ac.jp (remove space)
 */
 
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include "tp3.h"
 
 /* Period parameters */
 #define N 624
@@ -188,395 +193,55 @@ double genrand_res53(void)
 }
 /* These real versions are due to Isaku Wada, 2002/01/09 added */
 
-/***************** Debut question 2 ******************************/
-
-/* -------------------------------------------------------------- */
-/* uniform : retourne un nombre aléatoire entre deux réels a et b */
-/*                                                                */
-/* Entrée : deux réels a et b                                     */
-/* Sortie : un nombre aléatoire entre a et b                      */
-/* -------------------------------------------------------------- */
-double uniform(double a, double b)
+int init_mt()
 {
-    double rand = genrand_real2();
-    return a + (b - a) * rand;
-}
-
-/* -------------------------------------------------------------- */
-/* test_uniform : test differentes valeurs pour n de tirage       */
-/*                de la fct uniform(a, b)                         */
-/* Entrée : deux réels a et b                                     */
-/* Sortie : un nombre aléatoire entre a et b                      */
-/* -------------------------------------------------------------- */
-void test_uniform(float a, float b)
-{
-    int n = 1000000; //Nombre de tirage
-    float temp[n];   //Tableau stockant les differentes valeurs
-    float moyenne = 0;
-
-    float val = uniform(a, b);
-    temp[0] = val;
-
-    float max = val;
-    float min = val;
-
-    for (int i = 1; i < n; i++)
-    {
-        val = uniform(a, b);
-        temp[i] = val;
-        moyenne += val;
-        if (val > max)
-            max = val;
-        if (val < min)
-            val = min;
-    }
-    moyenne /= n;
-
-    printf("\nPour %d tirage sur une loi uniforme entre %.2f et %.2f\n", n, a, b);
-    printf("moyenne = %.2f, vrai moyenne (%.2f + %.2f)/2 = %.2f\n", moyenne, a, b, (a + b) / 2);
-    printf("max = %.2f\n", max);
-    printf("min = %.2f\n", min);
-    printf("\n");
-}
-/******************* Fin question 2 ******************************/
-
-/***************** Debut question 3 ******************************/
-
-/* -------------------------------------------------------------- */
-/* discrete_distribution : cree un distribution discrete 3classes */
-/*                         avec P(A)=0.5; P(B)=0.1; P(C)=0.4      */
-/* Entrée : entier n -> nb drawings                               */
-/* Sortie : rien                                                  */
-/* -------------------------------------------------------------- */
-void discrete_distribution(int n)
-{
-    double rand;
-    int i;
-
-    int population[3];
-    float proba[3];
-
-    //Initialise les differentes population a 0
-    for (i = 0; i < 3; i++)
-    {
-        population[i] = 0;
-    }
-
-    for (i = 0; i < n; ++i)
-    {
-        rand = genrand_real2();
-
-        if (rand < 0.5) //Appartient a la classe A
-        {
-            population[0]++;
-        }
-        else
-        {
-            if (rand < 0.6) // Appartient a la classe B
-            {
-                population[1]++;
-            }
-            else //Appartient a la classe C
-            {
-                population[2]++;
-            }
-        }
-    }
-
-    printf("\nn = %d drawings\n", n);
-    for (i = 0; i < 3; i++)
-    {
-        proba[i] = (float)population[i] / (float)n;
-        printf("population %d = %d et frequence = %f\n", i, population[i], proba[i]);
-    }
-}
-
-//Calcul et affiche la proba et proba_cumulee croissante d'une distribution
-void tab_observation(int taille, int observation[taille])
-{
-    int i;
-    int nb_observation = 0;
-    float proba[taille];
-    float proba_cumul[taille];
-
-    for (i = 0; i < taille; i++)
-    {
-        nb_observation += observation[i];
-    }
-    for (i = 0; i < taille; i++)
-    {
-        proba[i] = (float)observation[i] / (float)nb_observation;
-        proba_cumul[i] = proba[i];
-        if (i != 0)
-            proba_cumul[i] += proba_cumul[i - 1];
-    }
-
-    printf("\nIl y a %d observation.\n", nb_observation);
-    for (i = 0; i < taille; i++)
-    {
-        printf("proba[i]=%.3f et ", proba[i]);
-        printf("proba_cumul[i]=%.3f\n", proba_cumul[i]);
-    }
-}
-/******************* Fin question 3 ******************************/
-
-/***************** Debut question 4 ******************************/
-//Trace un histogramme
-/* --------------------------------------------------------------------- */
-/* trace_histo :  affiche un histogramme d'un tableau de valeurs         */
-/*                                                                       */
-/* Entrée :   un tableau d'entier                                        */
-/*            un entier : taille du tableau                              */
-/*            un entier : precision de l'unite de l'histo                */
-/* Sortie : rien                                                         */
-/* --------------------------------------------------------------------- */
-void trace_histo(int bin[], int nb_bin, int precision)
-{
-    printf("histogramme\n");
-    printf("unite/valeurs quantite*precision");
-    int quantite;
-    int tot = 0;
-
-    for (int i = 0; i < nb_bin; i++)
-    {
-        //Affiche l'axe des unites/valeurs
-        //printf("\n[%2d..%2d] ", i, i + 1); //exponentiel negative
-        //printf("\n%3d ", i);                 //somme lance de 20 des
-        printf("\n[%+.1f..%+.1f] ", (float)i / 10. - 1.0,
-                                    (float)i / 10. - 0.9); //Box Muller
-
-        quantite = bin[i] / precision;
-
-        printf("%3d*%d ", quantite, precision);
-
-        for (int j = 0; j < quantite; j++)
-        {
-            printf("■");
-        }
-    }
-    printf("\n");
-}
-
-/* -------------------------------------------------------------- */
-/* negExp :  f^(-1) d'une loi exponentielle négative uniforme     */
-/*                                                                */
-/* Entrée : un real : moyenne                                     */
-/* Sortie : un nb généré aléatoirement suivant                    */
-/*          une loi exp negative                                  */
-/* -------------------------------------------------------------- */
-double negExp(double mean)
-{
-    double rand = genrand_real2();
-    double tmp = 1. - rand;
-    return (-mean * log(tmp));
-}
-
-/* -------------------------------------------------------------- */
-/* testnegExp :  test negExp en affichant un histo                */
-/*                                                                */
-/* Entrée : un real : mean                                        */
-/*          un entier n : nombre de tirage                        */
-/*          un entier precision_histo                             */
-/* Sortie : rien                                                  */
-/* -------------------------------------------------------------- */
-void test_negExp(double mean, int n, int precision_histo)
-{
-    double average = 0.;
-    double tirage;
-    int bin[20]; // tableau comptant les valeurs entre un certain intervalle
-
-    //Initialisation du tableau
-    for (int k = 0; k < 20; k++)
-        bin[k] = 0;
-
-    for (int i = 0; i < n; ++i)
-    {
-        tirage = negExp(mean);
-        average += tirage;
-        if (tirage > 20.)
-        {
-            bin[19]++;
-        }
-        else
-            bin[(int)tirage]++;
-    }
-    average /= n;
-
-    printf("\n\nnegExp(%.3f) -> moyenne calculee est : %.3f pour %d tirages\n", mean, average, n);
-    trace_histo(bin, 20, precision_histo);
-}
-
-/******************* Fin question 4 ******************************/
-
-/******************* Debut question 5 ****************************/
-
-/************** Somme de 20 lances de des ***********************/
-
-/* -------------------------------------------------------------- */
-/* dice_6 :  genere un nb aleatoire uniforme entre 1 et 6         */
-/*                                                                */
-/* Entrée : rien                                                  */
-/*                                                                */
-/* Sortie : un entier : nb aleatoire uniforme entre 1 et 6        */
-/* -------------------------------------------------------------- */
-int dice_6()
-{
-    double rand = uniform(1, 7);
-    return (int)rand;
-}
-
-
-/* -------------------------------------------------------------- */
-/* dice_sum :  somme du resultat de m lance de des                */
-/*                                                                */
-/* Entrée : un entier : nombre de lance de de                     */
-/*                                                                */
-/* Sortie : un entier : somme du resultat de m lance de des       */
-/* -------------------------------------------------------------- */
-int dice_sum(int m)
-{
-    int dice = 0;
-
-    for (int i = 0; i < m; ++i)
-    {
-        dice += dice_6();
-    }
-    return dice;
-}
-
-//repete n fois l'experience de m lances de de avec somme du resultat
-/* -------------------------------------------------------------- */
-/* repeat_sum_dice :  repete plusieur fois la somme du res de des */
-/*                    trace l'histogramme des occurrences         */
-/*                    et calcul la moyenne                        */
-/*                                                                */
-/* Entrée : un entier n : nombre d'iteration de l'experience      */
-/*          un entier histo_precision : precision pour histo      */
-/*          un entier m : nombre de lance de des pour la somme    */
-/*                                                                */
-/* Sortie : rien                                                  */
-/* -------------------------------------------------------------- */
-void repeat_sum_dice(int n, int histo_precision, int m)
-{
-    int score_max = m * 6;     // Valeur maximale de score possible
-    int score[score_max + 1];  // Tableau comptant les occurences des scores
-    int moyenne = 0;           // Moyenne
-    
-    // Initialisation du tableau des occurences
-    for (int k = 0; k < score_max + 1; k++)
-    {
-        score[k] = 0;
-    }
-
-    int res_sum = 0;
-
-    // Effecture n fois la somme du resultat de m lance de des
-    for (int i = 0; i < n; ++i)
-    {
-        res_sum = dice_sum(m);
-        score[res_sum]++;
-        moyenne += res_sum;
-    }
-
-    moyenne /= n;
-
-    printf("\nSomme de m = %d des et n = %d iteretions", m, n);
-    printf("\nmoyenne calculee = %d\n", moyenne);
-    trace_histo(score, score_max + 1, histo_precision);
-}
-
-/************** Box muller ***********************/
-/* -------------------------------------------------------------- */
-/* box_muller :  methode de box et muller                         */
-/*                                                                */
-/* Entrée : un entier n : nombre d'iteration de l'experience      */
-/*          un entier histo_precision : precision pour histo      */
-/*                                                                */
-/* Sortie : 2 entier x1 et x2 suivant N(0,1)                      */
-/* -------------------------------------------------------------- */
-void box_muller(int n, int histo_precision)
-{
-    double rn1, rn2;
-    double x1, x2;
-
-    //Initialisation tableau de compteur
-    int test20bin[20];
-    for (int i = 0; i < 20; ++i)
-    {
-        test20bin[i] = 0;
-    }
-
-    for (int k = 0; k < n; k++)
-    {
-        //Calcul nombre aleatoire suivant une gaussienne N(0,1)
-        rn1 = genrand_real2();
-        rn2 = genrand_real2();
-        x1 = cos(2 * M_PI * rn2) * sqrt(-2 * log(rn1));
-        x2 = sin(2 * M_PI * rn2) * sqrt(-2 * log(rn1));
-
-        //Comptabilise les valeurs entre -1 et 1
-        if (x1 > -1 && x1 < 1)
-            test20bin[(int)((x1 + 1) * 10)]++;
-        if (x2 > -1 && x2 < 1)
-            test20bin[(int)((x2 + 1) * 10)]++;
-    }
-    printf("Box and Muller : N(0,1) \n");
-    printf("Avec %d tirages et une precision de %d -> ", n, histo_precision);
-    trace_histo(test20bin, 20, histo_precision);
-}
-
-/******************* Fin question 5 ******************************/
-
-int main(void)
-{
-/*
-    int i;
-    // Test pour comparaison avec fichier .out
-    unsigned long init[4]={0x123, 0x234, 0x345, 0x456}, length=4;
+    unsigned long init[4] = {0x123, 0x234, 0x345, 0x456}, length = 4;
     init_by_array(init, length);
+}
+
+int test_mt(void)
+{
+    int i;
+
     printf("1000 outputs of genrand_int32()\n");
-    for (i=0; i<1000; i++) {
-      printf("%10lu ", genrand_int32());
-      if (i%5==4) printf("\n");
+    for (i = 0; i < 1000; i++)
+    {
+        printf("%10lu ", genrand_int32());
+        if (i % 5 == 4)
+            printf("\n");
     }
     printf("\n1000 outputs of genrand_real2()\n");
-    for (i=0; i<1000; i++) {
-      printf("%10.8f ", genrand_real2());
-      if (i%5==4) printf("\n");
+    for (i = 0; i < 1000; i++)
+    {
+        printf("%10.8f ", genrand_real2());
+        if (i % 5 == 4)
+            printf("\n");
     }
-*/
+    return 0;
+}
+
+/************************ Fin MT *************************************/
+
+/********************* debut du tp4 **********************************/
+
+int main()
+{
+    init_mt();
+    //test_mt();
+    float debut, fin;
+
 /*
-    double a = -89.2;
-    double b = 56.7;
-    printf("Generate pseudo-random numbers between %3.2f and %3.2f : %3.2f \n", a, b, uniform(a, b));
-    test_uniform(a, b);
+    n = 1000000;
+    debut = clock();
 
-
-    discrete_distribution(1000);
-    discrete_distribution(1000000);
-
-    int tab[3] = {500, 100, 400};
-    tab_observation(3, tab);
+    fin = clock();
+    printf("temps d'execution %.5f s pour n = %d points\n\n", (fin-debut)/1000000 , n);
 */
-/*
-    printf("\n");
-
-    test_negExp(10, 1000, 10);
-    test_negExp(10, 1000000, 1000);
-*/
-/*
-    printf("\n");
-
-    repeat_sum_dice(1000000, 1000, 20);
-
-    printf("\n");
-*/
-
-    box_muller(1000000, 1000);
+   
 
     return 0;
 }
 
 /*
-gcc mt19937ar.c -o prog -lm
+gcc tp4.c -o prog -lm
 */
---
+++ tp4/tp4.h
@@ -0,0 +1,34 @@
+#ifndef TP_3_H
+#define TP_3_H
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*
+gcc tp3.c -o prog -lm
+option -O2  pour optimiser 
+*/
+/*
+./prog > res.txt ; diff res.txt mt19937ar.out ; echo $?
+*/
+
+// Fonction generateur MT
+void init_genrand(unsigned long s);
+void init_by_array(unsigned long init_key[], int key_length);
+unsigned long genrand_int32(void);
+long genrand_int31(void);
+double genrand_real1(void);
+double genrand_real2(void);
+double genrand_real3(void);
+double genrand_res53(void);
+
+int init_mt();
+int test_mt(void);
+
+// Mes fonction tp4
+
+
+
+#endif
