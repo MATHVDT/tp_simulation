1ac175de70ba6887e069134f4011cfe4c852de96
mavilledie4 <mathieu.villedieu_de_torcy@etu.uca.fr>
2021-10-25 15:27:57 +0200

    ajout fichier rabbit_simu

---
+++ rabbit_simu.c
@@ -0,0 +1,10 @@
+/* 
+TP Simulation
+Lab # 4a–A more realistic population growth 
+
+VILLEDIEU DE TORCY Mathieu
+ZZ2 F 2 G22
+*/
+
+#include "rabbit_simu.h"
+
---
+++ rabbit_simu.h
@@ -0,0 +1,19 @@
+/* 
+TP Simulation
+Lab # 4a–A more realistic population growth 
+
+VILLEDIEU DE TORCY Mathieu
+ZZ2 F 2 G22
+*/
+
+#ifndef RABBIT_SIMU_H
+#define RABBIT_SIMU_H
+
+
+typedef struct lapin
+{
+
+
+}lapin_t;
+
+#endif
