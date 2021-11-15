90aeb45c1d3865be7bcc58406d66f69847baefd6
mavilledie4 <mathieu.villedieu_de_torcy@etu.uca.fr>
2021-11-15 14:17:31 +0100

    ajout struct

--- tp4/rabbit_simu.h
+++ tp4/rabbit_simu.h
@@ -9,6 +9,8 @@ ZZ2 F 2 G22
 #ifndef RABBIT_SIMU_H
 #define RABBIT_SIMU_H
 
+#define POPULATION_MAX 1000000
+
 enum Sexe
 {
     male,
@@ -22,7 +24,7 @@ typedef struct lapin
 
 } lapin_t;
 
-type struct infoPop
+typedef struct infoPop
 {
     int nbMale;
     int nbFemelle;
@@ -31,5 +33,12 @@ type struct infoPop
     int nbTotal;
 
 } infoPop_t;
+
+typedef population
+{
+    lapin_t male[POPULATION_MAX];
+    lapin_t femelle[POPULATION_MAX];
+}
+population_t;
 
 #endif
